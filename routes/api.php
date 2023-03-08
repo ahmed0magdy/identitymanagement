b40b91cd9ae0fa01608e6a0ca2c9497536ccecdc
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2023-03-08 23:07:39 +0200

    https://nweave.atlassian.net/browse/REVIXIR-26 https://nweave.atlassian.net/browse/REVIXIR-42 https://nweave.atlassian.net/browse/REVIXIR-74
    work done
    ==========
    
    Added api routes and Auth controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/auth/google/redirect', [AuthApiController::class, 'googleredirect']);
+Route::get('/auth/google/callback', [AuthApiController::class, 'googlecallaback']);
