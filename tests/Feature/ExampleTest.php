4ef870506a9b0b75ec0069736b4ee3cd5078fc14
abdel_rhman <abdel_rahman@nweave.com>
2023-03-09 20:13:19 +0200

    Fix initial commit unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,7 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /** @test */
     public function testTheApplicationReturnsASuccessfulResponse()
     {
         $response = $this->get('/');
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /** @test */
     public function testThatTrueIsTrue()
     {
         $this->assertTrue(true);
