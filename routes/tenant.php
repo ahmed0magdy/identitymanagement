19bab71b408b6a0524a86ff669ad66dc24d892ec
Amr ibrahem <61279156+AmrIbrahem99@users.noreply.github.com>
2023-03-12 17:55:21 +0200

    https://nweave.atlassian.net/browse/REVIXIR-54 https://nweave.atlassian.net/browse/REVIXIR-76 work done ========
    Added sanctum routes and

--- routes/tenant.php
+++ routes/tenant.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
@@ -18,12 +20,35 @@ use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 |
 */
 
-Route::middleware([
-    'web',
+//override sanctum's csrf-cookie route to use tenancy initialization middleware
+Route::group(['prefix' => config('sanctum.prefix', 'sanctum')], static function () {
+    Route::get('/csrf-cookie', [CsrfCookieController::class, 'show'])
+        ->middleware([
+            'api',
+            InitializeTenancyByDomain::class // Use tenancy initialization middleware of your choice
+        ])->name('sanctum.csrf-cookie');
+});
+//universal auth routes for tenant and central domains (e.g. login, logout)
+Route::prefix('api')->middleware([
+    'universal',
     InitializeTenancyByDomain::class,
-    PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
+
+//tenant-only routes guarded by sanctum
+Route::prefix('api')->group(function () {
+    Route::middleware([
+        'auth:sanctum',
+        InitializeTenancyByDomain::class, // Use tenancy initialization middleware of your choice
+        PreventAccessFromCentralDomains::class, // Prevent access to central domains
+    ])->group(function () {
+        Route::get('/get', function () {
+            return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+        });
+    });
+});
+
