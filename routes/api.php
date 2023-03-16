3168d92800b024e1fa392bac37e07b41bfc34fc4
Marwa-nWeave <marwa.abdullah@nweave.com>
2023-03-16 18:12:54 +0200

    https://nweave.atlassian.net/browse/REVIXIR-53 https://nweave.atlassian.net/browse/REVIXIR-68 work done ===========
    updating unit tests for permissions module

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TenantController;
 
 /*
 |--------------------------------------------------------------------------
