7fd8922d0e5e59c22a0f666e9a634db9d153e083
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2023-04-03 23:43:20 +0200

    fix tenant test

--- database/migrations/tenant/2023_03_21_071316_add_ldap_columns_to_tenant_users_table.php
+++ database/migrations/tenant/2023_03_21_071316_add_ldap_columns_to_tenant_users_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLdapColumnsToUsersTable extends Migration
+class AddLdapColumnsToTenantUsersTable extends Migration
 {
     /**
      * Run the migrations.
