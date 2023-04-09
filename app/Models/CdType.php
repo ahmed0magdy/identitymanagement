a63dacacfe3fb079539bcc44e9d99eed4adbf6cc
Marwa-nWeave <marwa.abdullah@nweave.com>
2023-04-09 12:55:45 +0200

    https://nweave.atlassian.net/browse/REVIXIR-104 https://nweave.atlassian.net/browse/REVIXIR-133 work done ===========
    adding db migration and model for code type

---
+++ app/Models/CdType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CdType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'internalKey',
+        'displayName',
+        'description',
+        'internalShortCode',
+        'rwStatusCd',
+        'rwCreatedSessionID',
+        'rwCreatedDT',
+        'rwModifiedDT',
+        'rwModifiedSessionID'
+    ];
+}
---
+++ database/migrations/2023_04_09_090350_create_cd_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('cd_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('internalKey', 50)->nullable();
+            $table->string('displayName', 100);
+            $table->string('description', 254)->nullable();
+            $table->integer('internalShortCode')->nullable();
+            $table->tinyInteger('rwStatusCd');
+            $table->bigInteger('rwCreatedSessionID');
+            $table->dateTime('rwCreatedDT');
+            $table->dateTime('rwModifiedDT')->nullable();
+            $table->bigInteger('rwModifiedSessionID')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cd_types');
+    }
+};
